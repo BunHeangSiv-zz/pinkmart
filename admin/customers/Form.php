fd2e4e87a64c1bcf9805f1c451fac803ccec25af
Yoeng Yu <alienwarcc01@gamil.com>
2024-05-23 23:50:04 +0700

    Update

--- admin/customers/Form.php
+++ admin/customers/Form.php
@@ -22,21 +22,22 @@
                 <h1 class="text-xl font-bold text-pink-400">Add Staff Information</h1>
                 <div class="border-b border-1 border-pink-400 my-3"></div>
                 <form class="w-full">
-                    <div class="grid grid-cols-3 gap-4">
+                    <div class="grid grid-cols-3 gap-4 mb-4">
                         <div>
-                            <label for="txtname" class="block text-sm font-medium leading-6 text-pink-400">Customer's
+                            <label for="txtname" class="block text-lg font-medium leading-6 text-pink-400">Customer's
                                 Name</label>
                             <div class="mt-2">
                                 <input type="text" id="txtname" name="txtname" autocomplete="off"
-                                    class="block w-full  border-0 pl-2 py-2.5 text-pink-400 shadow-sm ring-1 ring-inset ring-pink-400 placeholder:text-pink-400 focus:outline-none focus:ring-2 focus:ring-pink-400 sm:text-sm sm:leading-6">
+                                    class="block w-full  border pl-2 py-2.5 text-pink-400 shadow-sm ring-1 ring-inset ring-pink-400 placeholder:text-pink-400 focus:outline-none focus:ring-2 focus:ring-pink-400 sm:text-lg sm:leading-6">
                             </div>
                         </div>
                         <div>
-                            <label for="products" class="block text-sm font-medium leading-6 text-pink-400">Customer's
+                            <label for="products" class="block text-lg font-medium leading-6 text-pink-400">Customer's
                                 Sex</label>
                             <div class="mt-2">
-                                <select id="customerid" name="customerid"
-                                    class="product-select mt-1 block w-full text-pink-400" required>
+                                <select id="sexid" name="sexid"
+                                    class="block w-full  border pl-2 py-2.5 text-pink-400 shadow-sm ring-1 ring-inset ring-pink-400 placeholder:text-pink-400 focus:outline-none focus:ring-2 focus:ring-pink-400 sm:text-lg sm:leading-6"
+                                    required>
                                     <?php
             while ($row = mysqli_fetch_assoc($result_sex)) {
                 echo "<option value='{$row['sexid']}'>{$row['sexen']}</option>";
@@ -46,37 +47,31 @@
                             </div>
                         </div>
                         <div>
-                            <label for="quantity2"
-                                class="block text-sm font-medium leading-6 text-pink-400">Quantity</label>
+                            <label for="txtaddress" class="block text-lg font-medium leading-6 text-pink-400">Customer's
+                                Address</label>
                             <div class="mt-2">
-                                <input type="text" id="quantity2" name="quantity" autocomplete="off"
-                                    class="block w-full  border-0 pl-2 py-2.5 text-pink-400 shadow-sm ring-1 ring-inset ring-pink-400 placeholder:text-pink-400 focus:outline-none focus:ring-2 focus:ring-pink-400 sm:text-sm sm:leading-6">
+                                <input type="text" id="txtaddress" name="txtaddress" autocomplete="off"
+                                    class="block w-full  border pl-2 py-2.5 text-pink-400 shadow-sm ring-1 ring-inset ring-pink-400 placeholder:text-pink-400 focus:outline-none focus:ring-2 focus:ring-pink-400 sm:text-lg sm:leading-6">
                             </div>
                         </div>
                         <div>
-                            <label for="quantity3"
-                                class="block text-sm font-medium leading-6 text-pink-400">Quantity</label>
+                            <label for="txtphone" class="block text-lg font-medium leading-6 text-pink-400">Phone
+                                Number</label>
                             <div class="mt-2">
-                                <input type="text" id="quantity3" name="quantity" autocomplete="off"
-                                    class="block w-full  border-0 pl-2 py-2.5 text-pink-400 shadow-sm ring-1 ring-inset ring-pink-400 placeholder:text-pink-400 focus:outline-none focus:ring-2 focus:ring-pink-400 sm:text-sm sm:leading-6">
+                                <input type="text" id="txtphone" name="txtphone" autocomplete="off"
+                                    class="block  w-full  border pl-2 py-2.5 text-pink-400 shadow-sm ring-1 ring-inset ring-pink-400 placeholder:text-pink-400 focus:outline-none focus:ring-2 focus:ring-pink-400 sm:text-lg sm:leading-6">
                             </div>
                         </div>
-                        <div>
-                            <label for="quantity4"
-                                class="block text-sm font-medium leading-6 text-pink-400">Quantity</label>
-                            <div class="mt-2">
-                                <input type="text" id="quantity4" name="quantity" autocomplete="off"
-                                    class="block w-full  border-0 pl-2 py-2.5 text-pink-400 shadow-sm ring-1 ring-inset ring-pink-400 placeholder:text-pink-400 focus:outline-none focus:ring-2 focus:ring-pink-400 sm:text-sm sm:leading-6">
-                            </div>
-                        </div>
-                        <div>
-                            <label for="quantity5"
-                                class="block text-sm font-medium leading-6 text-pink-400">Quantity</label>
-                            <div class="mt-2">
-                                <input type="text" id="quantity5" name="quantity" autocomplete="off"
-                                    class="block w-full  border-0 pl-2 py-2.5 text-pink-400 shadow-sm ring-1 ring-inset ring-pink-400 placeholder:text-pink-400 focus:outline-none focus:ring-2 focus:ring-pink-400 sm:text-sm sm:leading-6">
-                            </div>
-                        </div>
+                    </div>
+                    <div class="space-x-2">
+                        <button type="submit"
+                            class="reset-button bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4">
+                            Submit
+                        </button>
+                        <a href="#"
+                            class="inline-block bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 text-center no-underline hover:no-underline">
+                            Cancel
+                        </a>
                     </div>
                 </form>
             </div>
